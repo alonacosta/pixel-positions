3bc668785c8f9703a162b0d0f11b97402cc02941
Alona <alona.costa2@gmail.com>
2025-01-31 16:10:07 +0000

    30. Authentication, Post a job

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -46,7 +45,8 @@ class User extends Authenticatable
         ];
     }
 
-    public function employer() {
+    public function employer()
+    {
         return $this->hasOne(Employer::class);
     }
 }
--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -3,7 +3,11 @@
 <x-panel class="flex flex-col text-center">
     <div class="self-start text-sm">{{ $job->employer->name }}</div>
     <div class="py-8">
-        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">{{ $job->title }}</h3>
+        <h3 class="group-hover:text-blue-800 text-xl font-bold transition-colors duration-300">
+            <a href="{{ $job->url }}" target="_blank">
+                {{ $job->title }}
+            </a>
+        </h3>
         <p class="text-sm mt-4">{{ $job->salary }}</p>
     </div>
     <div class="flex justify-between items-center mt-auto">
@@ -13,7 +17,7 @@
                 @endforeach
         </div>
 
-        <x-employer-logo :width="42"/>
+        <x-employer-logo :employer="$job->employer" :width="42"/>
 {{--        <img src="https://picsum.photos/42/42" alt="" class="rounded-xl">--}}
     </div>
 </x-panel>
